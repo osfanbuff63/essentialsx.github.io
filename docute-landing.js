7968656e0333c2d96af05c294106a755242228dc
md678685 <mathew678@hotmail.co.uk>
2018-03-25 22:52:21 +0100

    Change unofficial wording to official

--- docute-landing.js
+++ docute-landing.js
@@ -2,7 +2,7 @@ const landing = {
     html: `
     <div id="landing" class="main content markdown-body">
         <p><img src="https://camo.githubusercontent.com/9ad178e5cf76a372d6aaee8bbdf13485fbc1d51b/68747470733a2f2f692e696d6775722e636f6d2f435034535a70422e706e67"></img></p>
-        <p><i>This is not an official EssentialsX site.</i></p>
+        <p><i>The official EssentialsX site.</i></p>
         <section>
             <a class="big wiki" href="#/_DocuteHome">Visit wiki</a>
             <a class="big download" href="#" @click="toggleDownloads">
