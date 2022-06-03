c63fac96d506d7f5401e2db63b9443b24557336f
osfanbuff63 <91388253+osfanbuff63@users.noreply.github.com>
2022-06-03 07:45:24 -0400

    Docute landing too
    essentialsx.cf appears to be a fake site (see Essentials#4925)

--- docute-landing.js
+++ docute-landing.js
@@ -2,7 +2,7 @@ const landing = {
     html: `
     <div id="landing" class="main content markdown-body">
         <div style="text-align: center">
-            <h3>We've moved! You can visit the new EssentialsX website at <a href="https://essentialsx.cf">essentialsx.cf</a>.</h3>
+            <h3>We've moved! You can visit the new EssentialsX website at <a href="https://essentialsx.net">essentialsx.net</a>.</h3>
         </div>
         <hr />
         <p><img src="https://camo.githubusercontent.com/9ad178e5cf76a372d6aaee8bbdf13485fbc1d51b/68747470733a2f2f692e696d6775722e636f6d2f435034535a70422e706e67"></img></p>
